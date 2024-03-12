7473b6239e3e38314cd1de07bb314acab9ca1694
shandytepe <shandy@pacmannai.com>
2024-03-12 13:55:05 +0700

    update dim_products data model

--- models/marts/core/dim_products.sql
+++ models/marts/core/dim_products.sql
@@ -1,31 +1,41 @@
 with stg_dim_products as (
-    select
+    select 
         product_id as nk_product_id,
-        name,
-        subcategory_id as nk_subcategory_id,
+        "name",
+        subcategory_id,
         price,
         stock
     from {{ ref("stg_mini_order__product") }}
 ),
 
-dim_subcategories as (
+stg_subcategory as (
+    select * 
+    from {{ ref("stg_mini_order__subcategory") }}
+),
+
+stg_category as (
     select *
-    from {{ ref("dim_subcategories") }}
+    from {{ ref("stg_mini_order__category") }}
 ),
 
 final_dim_products as (
-    select
+    select 
         {{ dbt_utils.generate_surrogate_key( ["nk_product_id"] ) }} as sk_product_id,
         sdp.nk_product_id,
-        sdp.name,
-        dsc.sk_subcategory_id,
+        sdp."name",
         sdp.price,
         sdp.stock,
+        sc."name" as category_name,
+        sc.description as category_desc,
+        ssc."name" as subcategory_name,
+        ssc.description as subcategory_desc,
         {{ dbt_date.now() }} as created_at,
         {{ dbt_date.now() }} as updated_at
     from stg_dim_products sdp
-    join dim_subcategories dsc
-        on dsc.nk_subcategory_id = sdp.nk_subcategory_id
+    inner join stg_subcategory ssc
+        on ssc.subcategory_id = sdp.subcategory_id
+    inner join stg_category sc
+        on sc.category_id = ssc.category_id
 )
 
 select * from final_dim_products
